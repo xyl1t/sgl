32faa3a85e38c451a5a01feda705a1ca20b3bbc9
Marat Isaw <marat.isaw23@gmail.com>
2022-05-28 19:43:37 +0200

    docs(demo): add description to demo function signature

--- src/demo.h
+++ src/demo.h
@@ -18,6 +18,27 @@ typedef struct mouse {
 } mouse;
 
 
+/**
+ * @brief demo function template
+ * @param currDemo current demo index
+ * @param buffer buffere to draw on
+ * @param m mouse info (coordinates, button click)
+ * @param k keyboard info
+ * @param cp list of control points for current demo (beginning at 0 for all demos)
+ * @param ccp current control point index
+ * @param time elapsed time between frames in ms
+ * @param init if the demo should initialize
+ * 
+ * the function should have this structure:
+ * DEMOS(demoName) {
+ *     if (init) {
+ *         // init stuff
+ *        return;
+ *     }
+ *
+ *     // demo code
+ * }
+ */
 #define DEMOS(name) void name(int currDemo, sglBuffer* buffer, const mouse* m, const uint8_t* k, sglPoint* cp, int ccp, uint32_t time, bool init)
 typedef DEMOS(demos_f);
 DEMOS(demos);
